0f51dbd2519e12246cc4e8294343203f7209d451
ghif93 <100542654+ghif93@users.noreply.github.com>
2022-06-28 18:29:48 -0300

    Add files via upload
    John's solution

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-const btnDecrease = document.getElementById("decrease");
+/*const btnDecrease = document.getElementById("decrease");
 const btnReset = document.getElementById("reset");
 const btnIncrease = document.getElementById("increase");
 const counterText = document.getElementById("counterText");
 var counterVar = 0;
-//addEventListener("click", function () {
 
 
 btnIncrease.addEventListener("click",function(){
@@ -46,3 +45,34 @@ function changeColor(num){
 		counterText.style.color="red";
 	}
 }
+
+ */
+
+const value = document.querySelector("#counterText");
+const btns = document.querySelectorAll(".btn");
+var count=0;
+
+
+btns.forEach(function (btn){
+	btn.addEventListener("click", function(e){
+		const styles = e.currentTarget.classList;
+
+		if(styles.contains("decrease")){
+			count--;
+		}else if(styles.contains("increase")){
+			count++;
+		}else{
+			count = 0;
+		}
+
+		if (count > 0){
+			value.style.color="green";
+		}else if(count < 0){
+			value.style.color="red";
+		}else{
+			value.style.color="hsl(209, 61%, 16%)";
+		}
+		value.textContent=count;
+	})
+});
+ 
